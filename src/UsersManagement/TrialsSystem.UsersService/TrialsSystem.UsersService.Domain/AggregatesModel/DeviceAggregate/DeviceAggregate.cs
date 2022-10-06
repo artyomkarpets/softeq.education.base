ca5d3248552fc553e24a39deddb54e2a51463ad4
Gleb Kolosov <gleb.kolosov@softeq.com>
2022-10-06 09:43:44 +0200

    Added Device Aggregate model.

---
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Domain/AggregatesModel/DeviceAggregate/DeviceAggregate.cs
@@ -0,0 +1,20 @@
+﻿namespace TrialsSystem.UsersService.Domain.AggregatesModel.DeviceAggregate
+{
+    public class DeviceAggregate
+    {
+        public DeviceAggregate(string id, string serialNumber, string model, DeviceType type, string firmwareVersion)
+        {
+            Id = id;
+            SerialNumber = serialNumber;
+            Model = model;
+            Type = type;
+            FirmwareVersion = firmwareVersion;
+        }
+
+        public string Id { get; set; }
+        public string SerialNumber { get; set; }
+        public string Model { get; set; }
+        public DeviceType Type { get; set; }
+        public string FirmwareVersion { get; set; }
+    }
+}
